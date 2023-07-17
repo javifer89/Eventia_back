ddba530d21bf53a5c5c9d80cdefd31a48c5c167a
ANA CACERES <acaceresrueda@gmail.com>
2023-07-17 09:56:31 +0200

    .env

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 // TODO: mover los datos a .env
 const pool = mysql.createPool({
-    host: '127.0.0.1', //este es el local host
-    user: 'root',
-    password: 'root',
-    port: 3306,
-    database: 'espacios_culturales',
+    host: process.env.HOSTDB,
+    user: process.env.USERDB,
+    password: process.env.PASSDB,
+    port: process.env.PORTDB,
+    database: process.env.espacios_culturales,
 });
 
 //12 necesito que el pool este disponible desde cualquier punto de mi app. creo una variable glogal para toda la app
