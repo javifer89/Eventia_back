dffe25dda6c5b58acad3a7c5999aa71d70567ea6
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-17 08:46:11 +0200

    config db

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
 //creamos la app de Express
-const express = require('express');
+const express = require("express");
 
 //configuración de la app express
 const app = express();
 
 app.use(express.json()); //para que express pueda parsearlo y meterlo en el body en formato json
 
-
-
 //configuración de las rutas
 
-app.get('/prueba', (req, res) => {
+app.get("/prueba", (req, res) => {
+  //Hacemos acciones varias
 
-    //Hacemos acciones varias
+  //RESPONDEMOS!!
 
-    //RESPONDEMOS!!
-
-    res.send('esto es una respuesta d prueba');
-})
+  res.send("esto es una respuesta d prueba");
+});
 
 // RUTAS
-app.use('/api', require('./routes/api'));
+app.use("/api", require("./routes/api"));
 
 module.exports = app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 // requerir la libreria
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // TODO: mover los datos a .env
 const pool = mysql.createPool({
-    host: '127.0.0.1', //este es el local host
-    user: 'root',
-    password: '198944',
-    port: 3306,
-    database: 'espacios_culturales',
+  host: "127.0.0.1", //este es el local host
+  user: "root",
+  password: "mysqlpassword",
+  port: 3306,
+  database: "espacios_culturales",
 });
 
 //12 necesito que el pool este disponible desde cualquier punto de mi app. creo una variable glogal para toda la app
