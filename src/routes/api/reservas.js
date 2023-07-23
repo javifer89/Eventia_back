527108b26559343e074c99e5a79bbe3e13722c6e
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-23 12:42:30 +0200

    corregidos metodos en las rutas de reservas

--- src/routes/api/reservas.js
+++ src/routes/api/reservas.js
@@ -4,18 +4,18 @@ const reservasController = require("../../controllers/reservas.controller");
 
 
 //GET
-router.get("/", reservasController.getAllReservas);
+router.get("/", reservasController.getAll);
 // router.get("/usuario/:usersala", /*checkToken,*/ reservasController.getByUsuario);
-router.get("/:id_reserva", /*checkToken,*/ reservasController.reservaById);
+router.get("/:id_reserva", /*checkToken,*/ reservasController.getById);
 
 //POST
 router.post("/", reservasController.create);
 
 
 //PUT
-router.put("/editar/:id_reserva", reservasController.updateById);
+router.put("/editar/:id_reserva", reservasController.update);
 
 //DELETE
-router.delete("/:id_reserva", reservasController.deleteById);
+router.delete("/:id_reserva", reservasController.remove);
 
 module.exports = router;
