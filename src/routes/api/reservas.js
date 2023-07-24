8e54bc4ae7492ba8848aad7db7e4c262c9551ed9
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-24 12:35:14 +0200

    checktoken a la creación de reserva

--- src/routes/api/reservas.js
+++ src/routes/api/reservas.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 
 const reservasController = require("../../controllers/reservas.controller");
 
-
+const { checkToken } = require("../../helpers/middlewares");
 //GET
 router.get("/", reservasController.getAll);
 // router.get("/usuario/:usersala", /*checkToken,*/ reservasController.getByUsuario);
 router.get("/:id_reserva", /*checkToken,*/ reservasController.getById);
 
 //POST
-router.post("/", reservasController.create);
+router.post("/", checkToken, reservasController.create);
 
 
 //PUT
